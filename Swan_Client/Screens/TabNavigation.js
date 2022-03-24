cc15bbb4f631f85da031620fcbaad624728875d9
Daria <32822@student.pwsztar.edu.pl>
2022-03-24 16:55:33 +0100

    Dodanie icon

--- Swan_Client/Screens/TabNavigation.js
+++ Swan_Client/Screens/TabNavigation.js
@@ -7,20 +7,90 @@ import Notification from './Notification';
 import Home from './Home';
 import Favourite from './Favourite';
 
+
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
 
 
-export default function TabNavigation({navigation}) {
+const TabNavigation = () => {
 
     return (
-        
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Favourite" component={Favourite} />
-            <Tab.Screen name="Notification" component={Notification} />
-            <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Navigator
+            tabBarOptions={{
+                showLabel: false,
+                style: {
+                    position: 'absolute',
+                    height: 70,
+                    backgroundColor: '#ffffff'
+                }
+            }}
+        >
+            <Tab.Screen name="Home" component={Home} options={{ 
+                headerShown: false,
+                tabBarIcon: ({focused}) => (
+                    <View style={{alignItems:'center', justifyContent:'center'}}>
+                        <Image
+                            source={require('../assets/img/home.png')}
+                            resizeMode='contain'
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? '#013946' : '#909090'
+                            }}
+                        />
+                    </View>
+                ),
+                }} />
+            <Tab.Screen name="Favourite" component={Favourite} options={{ 
+                headerShown: false,
+                tabBarIcon: ({focused}) => (
+                    <View style={{alignItems:'center', justifyContent:'center'}}>
+                        <Image
+                            source={require('../assets/img/favourite.png')}
+                            resizeMode='contain'
+                            style={{
+                                width: 23,
+                                height: 23,
+                                top: 1,
+                                tintColor: focused ? '#013946' : '#909090'
+                            }}
+                        />
+                    </View>
+                ),
+                }} />
+            <Tab.Screen name="Notification" component={Notification} options={{ 
+                headerShown: false,
+                tabBarIcon: ({focused}) => (
+                    <View style={{alignItems:'center', justifyContent:'center'}}>
+                        <Image
+                            source={require('../assets/img/bell.png')}
+                            resizeMode='contain'
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? '#013946' : '#909090'
+                            }}
+                        />
+                    </View>
+                ),
+                }} />
+            <Tab.Screen name="Profile" component={Profile} options={{ 
+                headerShown: false,
+                tabBarIcon: ({focused}) => (
+                    <View style={{alignItems:'center', justifyContent:'center'}}>
+                        <Image
+                            source={require('../assets/img/user.png')}
+                            resizeMode='contain'
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? '#013946' : '#909090'
+                            }}
+                        />
+                    </View>
+                ),
+                }} />
         </Tab.Navigator>
         
     )
@@ -35,7 +105,7 @@ const styles = StyleSheet.create({
         height: '100%',
         backgroundColor: '#ffffff'
     },
-
-
+    
 });
 
+export default TabNavigation;
