2d14d0ff08d2bd1cab20ead8078c49b95adb137b
Daria <32822@student.pwsztar.edu.pl>
2022-04-09 23:08:24 +0200

    Ekran Cart

--- Swan_Client/Screens/Cart.js
+++ Swan_Client/Screens/Cart.js
@@ -5,11 +5,19 @@ import logo from '../logo.png'
 
 export default function Cart({ navigation }) {
 
-    const [choice, setChoice] = useState([
-        { name: 'Bracelet', id: '1', price: '$ 50.00', image: require('../assets/img/star.png') },
-        { name: 'Rings', id: '2', price: '$ 250.00', image: require('../assets/img/ring.png') },
-        { name: 'Necklaces', id: '3', price: '$ 125.00', image: require('../assets/img/necklace.png') },
-        { name: 'Earrings', id: '4', price: '$ 200.00', image: require('../assets/img/earring.png') },
+    const [product, setProduct] = useState([
+        { name: 'Bracelet', id: '1', price: '$ 50.00', image: require('../assets/img/b1.jpg') },
+        { name: 'Ring', id: '2', price: '$ 250.00', image: require('../assets/img/r1.jpg') },
+        { name: 'Necklace', id: '3', price: '$ 125.00', image: require('../assets/img/n1.jpg') },
+        { name: 'Earrings', id: '4', price: '$ 200.00', image: require('../assets/img/e1.jpg') },
+        { name: 'Bracelet', id: '5', price: '$ 50.00', image: require('../assets/img/b1.jpg') },
+        { name: 'Ring', id: '6', price: '$ 250.00', image: require('../assets/img/r1.jpg') },
+        { name: 'Necklace', id: '7', price: '$ 125.00', image: require('../assets/img/n1.jpg') },
+        { name: 'Earrings', id: '8', price: '$ 200.00', image: require('../assets/img/e1.jpg') },
+        { name: 'Bracelet', id: '9', price: '$ 50.00', image: require('../assets/img/b1.jpg') },
+        { name: 'Ring', id: '28', price: '$ 250.00', image: require('../assets/img/r1.jpg') },
+        { name: 'Necklace', id: '90', price: '$ 125.00', image: require('../assets/img/n1.jpg') },
+
     ]);
 
     return (
@@ -22,47 +30,85 @@ export default function Cart({ navigation }) {
             </View>
             <SafeAreaView>
                 <ScrollView style={styles.list}>
-
-                    <View style={styles.cardContainer}>
-                        <View style={styles.card}>
-                            <TouchableOpacity style={styles.imgBox} onPress={() => navigation.navigate('Product')}>
-                                <Image
-                                    style={styles.img}
-                                    source={require('../assets/img/b1.jpg')}
-                                />
-                            </TouchableOpacity>
-
-                            <View style={styles.cardTextContainer}>
-                                <Text style={styles.cardTextTitle}>Bracelet</Text>
-                                <Text style={styles.cardTextPrice}>$ 50.00</Text>
-                                <View style={styles.counter}>
-                                    <TouchableOpacity style={styles.iconBox2}>
+                    <FlatList
+                        style={styles.list}
+                        keyExtractor={(item) => item.id}
+                        data={product}
+                        renderItem={({ item }) => (
+                            <View style={styles.cardContainer}>
+                                <View style={styles.card}>
+                                    <TouchableOpacity style={styles.imgBox} onPress={() => navigation.navigate('Product')}>
                                         <Image
-                                            style={styles.icon2}
-                                            source={require('../assets/img/add.png')}
+                                            style={styles.img}
+                                            source={item.image}
                                         />
                                     </TouchableOpacity>
-                                    <Text style={styles.textCounter}>0.1</Text>
-                                    <TouchableOpacity style={styles.iconBox2}>
-                                        <Image
-                                            style={styles.icon2}
-                                            source={require('../assets/img/minus.png')}
-                                        />
-                                    </TouchableOpacity>
+
+                                    <View style={styles.cardTextContainer}>
+                                        <Text style={styles.cardTextTitle}>{item.name}</Text>
+                                        <Text style={styles.cardTextPrice}>{item.price}</Text>
+                                        <View style={styles.counter}>
+                                            <TouchableOpacity style={styles.iconBox2}>
+                                                <Image
+                                                    style={styles.icon2}
+                                                    source={require('../assets/img/add.png')}
+                                                />
+                                            </TouchableOpacity>
+                                            <Text style={styles.textCounter}>01</Text>
+                                            <TouchableOpacity style={styles.iconBox2}>
+                                                <Image
+                                                    style={styles.icon2}
+                                                    source={require('../assets/img/minus.png')}
+                                                />
+                                            </TouchableOpacity>
+                                        </View>
+                                    </View>
+                                    <View style={styles.buttonCrossContainer}>
+                                        <TouchableOpacity style={styles.iconBox3}>
+                                            <Image
+                                                style={styles.icon3}
+                                                source={require('../assets/img/cancel.png')}
+                                            />
+                                        </TouchableOpacity>
+                                    </View>
                                 </View>
                             </View>
-                            <View style={styles.buttonCrossContainer}>
-                            <TouchableOpacity style={styles.iconBox3}>
-                                        <Image
-                                            style={styles.icon3}
-                                            source={require('../assets/img/cancel.png')}
-                                        />
-                                    </TouchableOpacity>
-                            </View>
+                        )}
+                    />
+                    <View style={styles.rowContainer}>
+                        <View style={styles.inputContainer}>
+                            <TextInput style={styles.input} />
                         </View>
+                        <TouchableOpacity style={styles.iconBox4}>
+                            <Image
+                                style={styles.icon4}
+                                source={require('../assets/img/right-arrow.png')}
+                            />
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styles.rowContainer}>
+                        <View style={styles.totalContainer}>
+                            <Text style={styles.total}>
+                                Total:
+                            </Text>
+                            <Text style={styles.price}>
+                                $ 375.00
+                            </Text>
+                        </View>
+                    </View>
+                    
+                    <View style={styles.btnContainer}>
+                    <TouchableOpacity style={styles.btn}>
+                        <Text style={styles.btntext}>Check out</Text>
+                    </TouchableOpacity>
+                    </View>
+                    
+                    <View style={styles.saveContainer}>
                     </View>
                 </ScrollView>
             </SafeAreaView>
+
         </View>
     )
 }
@@ -70,7 +116,6 @@ export default function Cart({ navigation }) {
 const styles = StyleSheet.create({
     body: {
         flex: 1,
-
         width: '100%',
         height: '100%',
         backgroundColor: '#ffffff'
@@ -114,11 +159,10 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
     },
     productCard: {
-        width: '334',
-        height: '100',
-
+        width: 334,
+        height: 100,
     },
-    
+
     counter: {
         // backgroundColor: 'green',
         display: 'flex',
@@ -127,7 +171,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         marginLeft: 115
     },
-    
+
     textCardTttle: {
         marginTop: 10,
         fontFamily: 'NunitoSans-Regular',
@@ -158,7 +202,6 @@ const styles = StyleSheet.create({
     card: {
         height: 120,
         width: 334,
-        // backgroundColor: 'green',
         flexDirection: 'row',
         shadowColor: '#8A959E',
         elevation: 0.5,
@@ -166,11 +209,13 @@ const styles = StyleSheet.create({
     cardTextContainer: {
         // backgroundColor: 'yellow',
         height: 120,
-        
+
     },
     cardContainer: {
         alignItems: 'center',
         justifyContent: 'center',
+        marginTop: 2,
+        height: 120,
     },
     counter: {
         // backgroundColor: 'green',
@@ -202,19 +247,19 @@ const styles = StyleSheet.create({
         marginLeft: 15,
         marginRight: 15
     },
-    cardTextTitle:{
+    cardTextTitle: {
         marginTop: 4,
         marginLeft: 2,
         color: '#000000',
         fontSize: 15,
     },
-    cardTextPrice:{
+    cardTextPrice: {
         fontSize: 15,
         marginLeft: 2,
         fontWeight: '600',
         color: '#000000',
     },
-    buttonCrossContainer:{
+    buttonCrossContainer: {
         height: '100%',
         width: '100%',
         marginLeft: 80,
@@ -230,5 +275,80 @@ const styles = StyleSheet.create({
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    inputContainer: {
+        elevation: 0.5,
+        shadowColor: '#8A959E',
+        height: 50,
+        width: 284,
+
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderBottomColor: '#E0E0E0',
+        margin: 20,
+        color: '#000000',
+    },
+    saveContainer: {
+        height: 60,
+        backgroundColor: '#ffffff',
+    },
+    rowContainer: {
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    iconBox4: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#013946',
+        borderRadius: 6,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    icon4: {
+        width: 18,
+        height: 18,
+        tintColor: '#ffffff'
+    },
+    totalContainer: {
+        height: 50,
+        width: 284,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    total: {
+        fontSize: 20,
+        color: '#000000',
+    },
+    price: {
+        fontSize: 20,
+        color: '#000000',
+        fontWeight: '500'
+    },
+    btn: {
+        height: 60,
+        width: 300,
+        backgroundColor: '#013946',
+        display:'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 4,
+        // position: 'absolute',
+        
+    },
+
+    btntext: {
+        color: '#ffffff',
+        fontFamily: 'NunitoSans-Regular',
+        fontSize: 18,
+    },
+    btnContainer:{
+        isplay:'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 20
     },
 });
